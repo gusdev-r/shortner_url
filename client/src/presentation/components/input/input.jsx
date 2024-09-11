4254fd539d42f89f49124edbafc47d53f8118c9b
Gustavo Moreira <125270361+gusdev-r@users.noreply.github.com>
2024-09-11 09:33:51 -0300

    feat: styles for url input

---
+++ client/src/presentation/components/input/input.jsx
@@ -0,0 +1,28 @@
+/* eslint-disable react/prop-types */
+import React from 'react'
+import { FormInputContainer, Label, Input, ErrorText } from './style'
+
+export default function FormInput({
+  type,
+  label,
+  placeholder,
+  errors = {},
+  register,
+  registerName,
+  ref,
+}) {
+  return (
+    <FormInputContainer>
+      <Label>{label}</Label>
+      <Input
+        type={type}
+        placeholder={placeholder}
+        {...register(registerName)}
+        red={ref}
+      />
+      {errors[registerName] && (
+        <ErrorText>{errors[registerName].message}</ErrorText>
+      )}{' '}
+    </FormInputContainer>
+  )
+}
